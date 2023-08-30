99f55549600bc7dfa317a993bbf51f728da8e446
dman <daniel.morrison@platform.sh>
2023-08-30 11:51:46 +0800

    cleanup messaging

--- src/Commands/PlatformshProjectCommands.php
+++ src/Commands/PlatformshProjectCommands.php
@@ -3,6 +3,7 @@
 namespace Drupal\platformsh_project\Commands;
 
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal;
 use Drush\Attributes\Argument;
 use Drush\Commands\DrushCommands;
 
@@ -23,27 +24,32 @@ class PlatformshProjectCommands extends DrushCommands {
    * Create a project from ProjectID.
    *
    * @command platformsh:create-project
-   * @param $project_id Project ID (Platformsh hash) to look up
+   *
+   * @param $project_id string Project ID (Platformsh hash) to look up
+   *
    * @aliases psh:create-project
    * @usage platformsh_project:create-project abcdefgh
    */
-  public function createProject($project_id, $options = []) {
+  public function createProject(string $project_id, $options = []) {
     $field_values = [
       'type' => 'project',
       'title' => $project_id,
-      'field_id' => $project_id
+      'field_id' => $project_id,
     ];
     $entity_type_id = 'node';
-    $this->output()->writeln(sprintf("Creating a project %s", $project_id));
-    $project = \Drupal::entityTypeManager()
+    $this->logger()->info(sprintf("Creating a project %s", $project_id));
+    $project = Drupal::entityTypeManager()
       ->getStorage($entity_type_id)
       ->create($field_values);
     // hook_entity_presave() will do a lookup to prepopulate new project info.
     $project->save();
     if ($project->id()) {
-      $this->output()->writeln(sprintf("Created a project %s", $project->getTitle()));
-    } else {
-      $this->output()->writeln(sprintf("Failed to create a project %s", $project->getTitle()));
+      $this->logger()
+        ->success(sprintf("Created a project %s", $project->getTitle()));
+    }
+    else {
+      $this->logger()
+        ->error(sprintf("Failed to create a project %s", $project->getTitle()));
     }
   }
 
@@ -57,9 +63,9 @@ class PlatformshProjectCommands extends DrushCommands {
   public function createTestContent() {
     $sample_ids = [
       'k5lc5wez3aso2' => "hello world",
-      'log5ehrvf6tjg' => "D7"
+      'log5ehrvf6tjg' => "D7",
     ];
-    $this->output()->writeln(sprintf("Creating sample projects"));
+    $this->logger()->info(sprintf("Creating sample projects"));
     foreach ($sample_ids as $project_id => $label) {
       $this->createProject($project_id);
     }
@@ -70,16 +76,20 @@ class PlatformshProjectCommands extends DrushCommands {
    *
    * @command platformsh:create-metric
    * @aliases psh:create-metric
-   * @param $project_id Project ID (platformsh hash) to attach metric to (must exist)
+   *
+   * @param $project_id Project ID (platformsh hash) to attach metric to (must
+   *   exist)
    * @param $metric_type Machine name of metric type ['ping', 'note', ...]
+   *
    * @usage drush platformsh_project:create-metric abcdefg ping
    */
   public function createMetric($project_id, $metric_type, $options = []) {
     $this->output()->writeln(sprintf("Creating metric for project"));
     // Find the project object that the project_id refers to.
     $project = platformsh_project_get_project_by_project_id($project_id);
-    if (! $project) {
-      $this->output()->writeln(sprintf("Failed to find a project with ID %s. aborting.",   $project_id));
+    if (!$project) {
+      $this->logger()
+        ->error(sprintf("Failed to find a project with ID %s. aborting.", $project_id));
       return;
     }
 
@@ -88,18 +98,22 @@ class PlatformshProjectCommands extends DrushCommands {
       'target' => ['target_id' => $project->id()],
     ];
     $entity_type_id = 'metric';
-    $this->output()->writeln(sprintf("Creating a %s metric for %s", $metric_type, $project_id));
+    $this->logger()
+      ->info(sprintf("Creating a %s metric for %s", $metric_type, $project_id));
+
     /** @var \Drupal\platformsh_project\Entity\Metric $metric */
-    $metric = \Drupal::entityTypeManager()
+    $metric = Drupal::entityTypeManager()
       ->getStorage($entity_type_id)
       ->create($field_values);
     $metric->save();
     $bundle_label = $metric->bundle->entity->label();
     if ($metric->id()) {
-      $this->output()->writeln(sprintf("Created a metric %s", $bundle_label));
-      $this->output()->writeln(print_r($metric->get('target')->getValue(), true) );
-    } else {
-      $this->output()->writeln(sprintf("Failed to create a metric %s", $bundle_label));
+      $this->logger()->success(sprintf("Created a metric %s", $bundle_label));
+      $this->logger()->info(print_r($metric->get('target')->getValue(), TRUE));
+    }
+    else {
+      $this->logger()
+        ->error(sprintf("Failed to create a metric %s", $bundle_label));
     }
 
   }
@@ -107,12 +121,14 @@ class PlatformshProjectCommands extends DrushCommands {
   /**
    * @command platformsh:reset-fields
    * @aliases psh:reset-fields
+   * @bootstrap full
    * @usage platformsh:reset-fields
    *   Reset the fields attached to metric entites to factory settings.
    *   Apply schema updates to the fields if they already exist.
    */
   public function resetFields() {
     platformsh_project_update_fields();
+    $this->logger()->success("Reset the fields");
   }
 
 }
